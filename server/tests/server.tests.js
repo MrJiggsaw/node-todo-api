8c4ff02b99fa023568c13263e743f58257f3cad3
Amanpreet999 <31345679+Amanpreet999@users.noreply.github.com>
2019-01-05 18:07:32 +0530

    ID test

--- server/tests/server.tests.js
+++ server/tests/server.tests.js
@@ -4,11 +4,14 @@ const request = require('supertest');
 const {app} = require('./../server');
 const {Todo} = require('./../models/todo');
 
+const {ObjectID} = require('mongodb');
 const todos = [
   {
+    _id : new ObjectID(),
     text : "Eat the pancakes"
   },
   {
+    _id : new ObjectID(),
     text : "Drink the milk"
   }
 ]
@@ -70,4 +73,30 @@ describe("GET /todos" ,() => {
       })
       .end(done);
   })
-})
+});
+
+describe('GET /todos/:id' ,() => {
+  it('should return todo doc' ,(done) => {
+    request(app)
+      .get(`/todos/${todos[0]._id.toHexString()}`)
+      .expect(200)
+      .expect((res) => {
+        expect(res.body.text).toBe(todos[0].test)
+      })
+      .end(done);
+  });
+
+  it('should return 404 if todo not found' , (done) => {
+    request(app)
+      .get(`/todos/${new ObjectID().toHexString()}`)
+      .expect(404)
+      .end(done);
+  });
+
+  it('should return 404 for non-object ids' ,(done) => {
+    request(app)
+      .get('/todos/123')
+      .expect(404)
+      .end(done);
+  });
+});
