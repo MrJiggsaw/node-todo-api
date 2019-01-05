ce68d1297ba289d983b6e1624b7a677d9d5df145
Amanpreet999 <31345679+Amanpreet999@users.noreply.github.com>
2019-01-05 17:25:37 +0530

    Add GET todos/:id

--- server/server.js
+++ server/server.js
@@ -5,6 +5,8 @@ const {mongoose} = require('./db/mongoose');
 const {Todo} = require('./models/todo');
 const {User} = require('./models/users');
 
+var {ObjectID} = require('mongodb');
+
 var port = 3000;
 var app = express();
 
@@ -28,7 +30,24 @@ app.get('/todos' ,(req, res) => {
   } , (err) => {
     res.status(400).send('Not Found');
   });
-})
+});
+
+app.get('/todos/:id' , (req , res) => {
+  var id  = req.params.id;
+  if (!ObjectID.isValid(id)){
+    res.status(404).send('Id Invalid.Check Again');
+  }
+  Todo.findById(id).then((todo) => {
+    if (todo){
+      return res.send({todo});
+    }
+    res.status(404).send();
+  }, (err) => {
+    if(err) return res.status(400).send();
+  });
+});
+
+
 app.listen(port , () => {
   console.log(`Started on port ${port}`);
 });
